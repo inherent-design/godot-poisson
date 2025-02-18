17f96773bc0f8062e1dee1a3a97586e7361112a6
mannie-exe <manpreet.mannie.singh@gmail.com>
2025-02-17 21:19:10 -0800

    Fix `RandomNumberGenerator.rand_poisson` param name

--- core/math/random_number_generator.cpp
+++ core/math/random_number_generator.cpp
@@ -43,7 +43,7 @@ void RandomNumberGenerator::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("randf_range", "from", "to"), &RandomNumberGenerator::randf_range);
 	ClassDB::bind_method(D_METHOD("randi_range", "from", "to"), &RandomNumberGenerator::randi_range);
 	ClassDB::bind_method(D_METHOD("rand_weighted", "weights"), &RandomNumberGenerator::rand_weighted);
-	ClassDB::bind_method(D_METHOD("rand_poisson", "weights"), &RandomNumberGenerator::rand_poisson);
+	ClassDB::bind_method(D_METHOD("rand_poisson", "lambda"), &RandomNumberGenerator::rand_poisson);
 	ClassDB::bind_method(D_METHOD("randomize"), &RandomNumberGenerator::randomize);
 
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "seed"), "set_seed", "get_seed");
